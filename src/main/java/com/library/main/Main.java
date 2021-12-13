7ff17b6a42e729d57ccd460aa224ab6d8886017c
seifgamal <seifgamal007@gmail.com>
2021-12-13 15:40:12 +0200

    fix: working code for main class to fix Book is already defined

--- src/main/java/com/library/main/Main.java
+++ src/main/java/com/library/main/Main.java
@@ -24,6 +24,38 @@ public class Main {
 		System.out.println(book);
 		bookController.deleteBookById(3);
 
+		/*
+
+		Book newBook = new Book();
+		newBook.setName("Seif Book");
+		newBook.setDescription("For Seif");
+		newBook.setRate(4.9);
+		newBook.setAvailable(true);
+		newBook.setAuthorId(1);
+
+		bookController.addBook(newBook);
+
+
+		Book updatedBook = bookController.getBookById(4);
+		updatedBook.setName("Abo Treka updated");
+		updatedBook.setDescription(updatedBook.getDescription());
+		updatedBook.setAuthorId(2);
+		bookController.updateBook(updatedBook);
+		*/
+		/*
+		Author newAuthor = new Author();
+		newAuthor.setEmail("s@gmail.com");
+
+		ZoneId defaultZoneId = ZoneId.systemDefault();
+		LocalDate localDate = LocalDate.of(1996, 4, 17);
+		Date date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+
+		newAuthor.setBirthdate(date);
+		newAuthor.setName("Seif Gamal");
+		newAuthor.setBio("It's me");
+		AuthorController authorController = new AuthorController();
+		 */
+
 		List<Book> booksAuthor = bookController.getBooksOfAuthor(2);
 		for (Book book: booksAuthor) {
 			System.out.println(book);
