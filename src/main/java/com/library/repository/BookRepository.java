517c2e1a8055f4168528c49287d0b84c55184e38
seifgamal <seifgamal007@gmail.com>
2022-11-09 19:09:00 +0400

    use JPARepository interface

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -1,41 +1,7 @@
 package com.library.repository;
 
 import com.library.model.Book;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-@Transactional
-@NoArgsConstructor
-public class BookRepository {
-
-	@PersistenceContext
-	EntityManager entityManager;
-
-	public Book add(Book book) {
-		return entityManager.merge(book);
-	}
-
-	public Book update(Book book) {
-		return entityManager.merge(book);
-	}
-
-	public List<Book> getAll() {
-		TypedQuery<Book> getAll = entityManager.createNamedQuery("get_all_books", Book.class);
-		return getAll.getResultList();
-	}
-
-	public Book getById(int bookId) {
-		return entityManager.find(Book.class, bookId);
-	}
-
-	public void deleteById(int bookId) {
-		Book book = entityManager.find(Book.class, bookId);
-		entityManager.remove(book);
-	}
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/library/service/BookService.java
+++ src/main/java/com/library/service/BookService.java
@@ -1,55 +1,58 @@
 package com.library.service;
 
+import com.library.exception.Book.BookNotFoundException;
 import com.library.model.Book;
 import com.library.repository.BookRepository;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class BookService {
 
-	@Autowired
-	private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-	public Book addBook(Book newBook) {
-		return bookRepository.add(newBook);
-	}
+    public Book addBook(Book newBook) {
+        return bookRepository.save(newBook);
+    }
 
-	public List<Book> getAllBooks() {
-		return bookRepository.getAll();
-	}
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 
-	public List<Book> filterListOfBooksByCategory(@NotNull List<Book> books, Integer categoryId) {
-		return books.stream().filter(b -> b.getCategoryId() == categoryId).collect(Collectors.toList());
-	}
+    public List<Book> filterListOfBooksByCategory(@NotNull List<Book> books, Integer categoryId) {
+        return books.stream().filter(b -> b.getCategoryId() == categoryId).collect(Collectors.toList());
+    }
 
-	public List<Book> filterListOfBooksByAuthor(@NotNull List<Book> books, Integer authorId) {
-		return books.stream().filter(b -> b.getAuthorId() == authorId).collect(Collectors.toList());
-	}
+    public List<Book> filterListOfBooksByAuthor(@NotNull List<Book> books, Integer authorId) {
+        return books.stream().filter(b -> b.getAuthorId() == authorId).collect(Collectors.toList());
+    }
 
-	public Book getBookById(int id) {
-		return bookRepository.getById(id);
-	}
+    public Book getBookById(Long id) throws BookNotFoundException {
+        return bookRepository
+                .findById(id)
+                .orElseThrow(() -> new BookNotFoundException());
+    }
 
-	public Book updateBook(Book updatedBook, int id) {
-		Book oldBook = getBookById(id);
-		if (oldBook == null) {
-			return null;
-		}
+    public Book updateBook(Book updatedBook, Long id) throws BookNotFoundException {
+        Book oldBook = getBookById(id);
+        if (oldBook == null) {
+            return null;
+        }
 
-		oldBook.setName(updatedBook.getName());
-		oldBook.setDescription(updatedBook.getDescription());
-		oldBook.setAvailable(updatedBook.isAvailable());
+        oldBook.setName(updatedBook.getName());
+        oldBook.setDescription(updatedBook.getDescription());
+        oldBook.setAvailable(updatedBook.isAvailable());
 
-		addBook(oldBook);
-		return oldBook;
-	}
+        addBook(oldBook);
+        return oldBook;
+    }
 
-	public void deleteBookById(int id) {
-		bookRepository.deleteById(id);
-	}
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
